a1d2b46d9c49ac52049b6ec4ba7cc7c8357c4ff7
Çağrı KİBAR <77233335+CagriKibar@users.noreply.github.com>
2021-04-10 17:29:02 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Writer
+    {
+        public int WriteId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterLastName { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+    }
+}
