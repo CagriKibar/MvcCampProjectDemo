f973e70ec0d94af92352130b8e3fa9acf4467ff2
Çağrı KİBAR <77233335+CagriKibar@users.noreply.github.com>
2021-04-24 00:03:03 +0300

    19. Ders Sonu

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+        public List<Category> GetAll()
+        {
+            return repository.List();
+        }
+        
+    }
+}
