a7510f81a6e91f1f61122c52608b2ef13947fb4f
Çağrı KİBAR <77233335+CagriKibar@users.noreply.github.com>
2021-05-16 15:28:54 +0300

    44. Ders Sonu

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+  public  class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(p => p.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz");
+            RuleFor(p => p.WriterLastName).NotEmpty().WithMessage("Yazar  Soy Adını Boş Geçemezsiniz");
+            RuleFor(p => p.WriterAbout).NotEmpty().WithMessage("Hakkımda Kısmını  Boş Geçemezsiniz");
+            RuleFor(p => p.WriterLastName).MinimumLength(2).WithMessage("Lütfen En Az 2 Karakter Girişi Yapınız");
+            RuleFor(p => p.WriterLastName).MaximumLength(50).WithMessage("Lütfen 50 Karakterden Fazla Değer Girişi Yapınız");
+
+
+
+        }
+    }
+}
--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -20,13 +20,17 @@ namespace DataAccessLayer.Concrete.Repositories
         }
         public void Add(T p)
         {
-            _object.Add(p);
+            var addedEntity = context.Entry(p);
+            addedEntity.State = EntityState.Added;
+           // _object.Add(p);
             context.SaveChanges();
         }
 
         public void Delete(T p)
         {
-            _object.Remove(p);
+            var deletedEntity = context.Entry(p);
+            deletedEntity.State = EntityState.Deleted;
+            //_object.Remove(p);
             context.SaveChanges();
         }
 
@@ -47,7 +51,8 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Update(T p)
         {
-            
+            var updatedEntity = context.Entry(p);
+            updatedEntity.State = EntityState.Modified;
             context.SaveChanges();
         }
     }
